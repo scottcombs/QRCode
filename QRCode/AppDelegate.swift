af2b237bc61035c6db7ba3c5009726aab81e532f
scottcombs <scott@nutznboltllc.com>
2018-12-22 16:58:39 -0700

    Initial Commit

---
+++ QRCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QRCode
+//
+//  Created by Scott on 12/22/18.
+//  Copyright © 2018 Nutz & Boltz Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
